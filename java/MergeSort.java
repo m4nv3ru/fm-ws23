1778b1905fab239b3b06eada3ecbc96bb4e40195
Michael Kurz <michael_kurz@posteo.at>
2023-12-07 16:33:43 +0100

    add java setup

---
+++ java/MergeSort.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+
+class Main {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Usage: MergeSort.jar <numbers>");
+            System.exit(1);
+        }
+
+
+        System.out.println("Hello, World!");
+    }
+}

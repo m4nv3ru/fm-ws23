6fbf061598f2e9b38d69f7d160fb01a469a00098
Michael Kurz <michael_kurz@posteo.at>
2023-12-07 16:57:43 +0100

    implement sorting algorithm in cpp and java

--- cpp/merge_sort.cpp
+++ cpp/merge_sort.cpp
@@ -1,13 +1,103 @@
-#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 
-int main(int argc, char const *argv[])
-{
-    if (argc < 2) {
-        printf("Usage: MergeSort <file>\n");
+std::vector<int> merge(const std::vector<int>& left, const std::vector<int>& right) {
+    std::vector<int> merged;
+    size_t leftIndex = 0, rightIndex = 0;
+
+    while (leftIndex < left.size() && rightIndex < right.size()) {
+        if (left[leftIndex] <= right[rightIndex]) {
+            merged.push_back(left[leftIndex++]);
+        } else {
+            merged.push_back(right[rightIndex++]);
+        }
+    }
+
+    while (leftIndex < left.size()) {
+        merged.push_back(left[leftIndex++]);
+    }
+
+    while (rightIndex < right.size()) {
+        merged.push_back(right[rightIndex++]);
+    }
+
+    return merged;
+}
+
+std::vector<int> mergeSort(const std::vector<int>& data) {
+    if (data.size() <= 1) {
+        return data;
+    }
+
+    size_t mid = data.size() / 2;
+    std::vector<int> left(data.begin(), data.begin() + mid);
+    std::vector<int> right(data.begin() + mid, data.end());
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    return merge(left, right);
+}
+
+std::vector<int> readData(const std::string& filename) {
+    std::vector<int> data;
+    std::ifstream file(filename);
+    std::string line;
+
+    if (file.is_open()) {
+        std::getline(file, line); // Skip the first line
+        std::getline(file, line);
+        std::stringstream ss(line);
+        std::string number;
+
+        while (std::getline(ss, number, ';')) {
+            data.push_back(std::stoi(number));
+        }
+    }
+
+    file.close();
+    return data;
+}
+
+void writeData(const std::string& filename, const std::vector<int>& data) {
+    std::ofstream file(filename);
+
+    if (file.is_open()) {
+        file << data.size() << std::endl;
+        for (size_t i = 0; i < data.size(); ++i) {
+            file << data[i];
+            if (i < data.size() - 1) {
+                file << ";";
+            }
+        }
+    }
+
+    file.close();
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2 || argc > 3) {
+        std::cout << "Usage: " << argv[0] << " <file> [write_output]" << std::endl;
         return 1;
     }
 
-    // Your code for merge_sort will go here
+    std::string filePath = argv[1];
+    bool writeOutput = argc == 3 && std::string(argv[2]) == "true";
+
+    if (filePath.substr(filePath.find_last_of(".") + 1) != "txt") {
+        std::cout << "File must be a .txt file" << std::endl;
+        return 1;
+    }
+
+    std::vector<int> data = readData(filePath);
+    std::vector<int> sorted = mergeSort(data);
+
+    if (writeOutput) {
+        writeData("result.txt", sorted);
+    }
 
     return 0;
 }
--- java/MergeSort.java
+++ java/MergeSort.java
@@ -1,14 +1,83 @@
+import java.io.*;
 import java.util.Arrays;
 
+public class MergeSort {
 
-class Main {
+    public static int[] merge(int[] left, int[] right) {
+        int[] merged = new int[left.length + right.length];
+        int leftIndex = 0, rightIndex = 0, mergeIndex = 0;
+
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex] <= right[rightIndex]) {
+                merged[mergeIndex++] = left[leftIndex++];
+            } else {
+                merged[mergeIndex++] = right[rightIndex++];
+            }
+        }
+
+        while (leftIndex < left.length) {
+            merged[mergeIndex++] = left[leftIndex++];
+        }
+
+        while (rightIndex < right.length) {
+            merged[mergeIndex++] = right[rightIndex++];
+        }
+
+        return merged;
+    }
+
+    public static int[] mergeSort(int[] data) {
+        if (data.length <= 1) {
+            return data;
+        }
+
+        int mid = data.length / 2;
+        int[] left = Arrays.copyOfRange(data, 0, mid);
+        int[] right = Arrays.copyOfRange(data, mid, data.length);
+
+        left = mergeSort(left);
+        right = mergeSort(right);
+
+        return merge(left, right);
+    }
+
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Usage: MergeSort.jar <file>");
+        if (args.length < 1 || args.length > 2) {
+            System.out.println("Usage: java MergeSort <file> [write_output]");
             System.exit(1);
         }
 
+        String filePath = args[0];
+        boolean writeOutput = args.length == 2 && args[1].equals("true");
 
-        System.out.println("Hello, World!");
+        if (!filePath.endsWith(".txt")) {
+            System.out.println("File must be a .txt file");
+            System.exit(1);
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            reader.readLine(); // Skip first line
+            String[] data = reader.readLine().split(";");
+            int[] intData = new int[data.length];
+            for (int i = 0; i < data.length; i++) {
+                intData[i] = Integer.parseInt(data[i].trim());
+            }
+            reader.close();
+
+            int[] sorted = mergeSort(intData);
+
+            if (writeOutput) {
+                BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"));
+                writer.write(sorted.length + "\n");
+                for (int i = 0; i < sorted.length; i++) {
+                    writer.write(sorted[i] + (i < sorted.length - 1 ? ";" : ""));
+                }
+                writer.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

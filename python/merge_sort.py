a08379da19189741617056673cbcaf2ef9a0a3c8
Michael Kurz <michael_kurz@posteo.at>
2023-12-07 16:46:54 +0100

    add output file functionality to python

--- python/merge_sort.py
+++ python/merge_sort.py
@@ -33,24 +33,34 @@ def merge_sort(data: list) -> list:
     return merge(left, right)
 
 
-def main(file_path: str):
+def main(file_path: str, write_output: bool = False):
     with open(file_path, 'r') as f:
         int(f.readline())
         data = f.read().split(';')
         data = list(map(int, data))
 
-    merge_sort(data)
+    sorted = merge_sort(data)
+
+    if write_output:
+        with open('result.txt', 'w') as f:
+            f.write(str(len(sorted)) + '\n')
+            f.write(';'.join(map(str, sorted)))
 
 
 if __name__ == '__main__':
     # Read file given as first argument and split it into values separated by ;
 
-    if len(sys.argv) != 2:
-        print('Usage: python merge_sort.py <file>')
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print('Usage: python merge_sort.py <file> [write_output]')
         sys.exit(1)
+
+    if len(sys.argv) == 3:
+        write_output = sys.argv[2] == 'true'
+    else:
+        write_output = False
 
     if not sys.argv[1].endswith('.txt'):
         print('File must be a .txt file')
         sys.exit(1)
 
-    main(sys.argv[1])
+    main(sys.argv[1], write_output)
